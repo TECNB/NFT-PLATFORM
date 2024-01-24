20c620c6d2ba55810bffb9e85c992eb3cad906fa
TECNB <3489044730@qq.com>
2024-01-24 13:48:47 +0800

    feat(StatisticsView.vue): 增加系列统计信息中关系排名的UI以及部分功能
    - 增加排行内容UI
    - 修改展示方式为动态数组
    - 增加排行筛选部分的UI
    - 实现切换跳转页面

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -15,31 +15,35 @@
 
         </div>
         <FilterSection from="StatisticsView"/>
+        <!-- 如果关注列表时去掉前方的序号 -->
         <div class="StatisticsContent">
             <div class="StatisticsContentTitle">
+                <p style="flex: 1;" v-if="TypeIndex.index !== 2">#</p>
                 <p style="flex: 12;">系列</p>
                 <p style="flex: 2;text-align: end;">交易量</p>
                 <p style="flex: 2;text-align: end;">变化</p>
                 <p style="flex: 2;text-align: end;">地板价</p>
                 <p style="flex: 2;text-align: end;">销售</p>
+                <!-- 下面的部分是用于放置图标做出的空缺 -->
                 <p style="flex: 1;text-align: end;"></p>
             </div>
-            <div class="StatisticsContentDetail">
+            <div class="StatisticsContentDetail" v-for="(item, index) in recommendedCollections" :key="index">
+                <div style="flex: 1;text-align: start;" v-if="TypeIndex.index !== 2">{{index+1}}</div>
                 <div class="StatisticsContentDetailName" style="flex: 12;">
                     <div style="flex: 1;">
-                        <img src="https://i.seadn.io/gcs/files/cd3e28d133070f314c751ccb1291a532.jpg?auto=format&dpr=1&h=500&fr=1 1x, https://i.seadn.io/gcs/files/cd3e28d133070f314c751ccb1291a532.jpg?auto=format&dpr=1&h=500&fr=1 2x"
+                        <img :src="item.imageUrl"
                             alt=""
                             style="height: 100%; width: 100%; border-radius: 20px; object-fit: cover; aspect-ratio: 1/1;">
                     </div>
                     <div style="flex: 11;">
-                        <p style="text-align: start;padding-left: 25px;">Bored Ape Yacht Club</p>
+                        <p style="text-align: start;padding-left: 25px;">{{ item.title }}</p>
                     </div>
 
                 </div>
-                <div style="flex: 2;text-align: end;">51ETH</div>
-                <div style="flex: 2;text-align: end;">+101%</div>
-                <div style="flex: 2;text-align: end;">26 ETH</div>
-                <div style="flex: 2;text-align: end;">2</div>
+                <div style="flex: 2;text-align: end;">{{item.price}}</div>
+                <div style="flex: 2;text-align: end;">{{item.price}}</div>
+                <div style="flex: 2;text-align: end;">{{item.price}}</div>
+                <div style="flex: 2;text-align: end;">{{item.price}}</div>
                 <div style="flex: 1;" class="icon"><el-icon><Delete /></el-icon></div>
             </div>
         </div>
@@ -51,6 +55,7 @@
 import { } from "vue"
 import MainNavbar from '../components/MainNavbar.vue'
 import FilterSection from '../components/FilterSection.vue'
+import { Collection } from '../interfaces/Collection';
 
 
 import { SelectedTypeIndexStore } from '../stores/SelectedIndexStore'
@@ -62,6 +67,54 @@ const selectType = (index: number) => {
 
     TypeIndex.index = index;
 };
+
+// TODO: 从后端获取数据
+// 如果TypeIndex.index === 0时则显示热门
+// recommendedCollections=
+// 如果TypeIndex.index === 1时则显示最佳
+// recommendedCollections=
+// 如果TypeIndex.index === 2时则显示关注列表
+// recommendedCollections=
+
+// 初始值
+const recommendedCollections: Collection[] = [
+    {
+        imageUrl: 'https://i.seadn.io/s/raw/files/6662e4fbea8ad15eb84990bc68351d57.png?auto=format&dpr=1&h=500&fr=1 1x, https://i.seadn.io/s/raw/files/6662e4fbea8ad15eb84990bc68351d57.png?auto=format&dpr=1&h=500&fr=1 2x',
+        title: 'Mint Genesis NFT',
+        price: '0.01 ETH',
+        tradingVolume: '68 ETH',
+    },
+    {
+        imageUrl: 'https://i.seadn.io/gae/WRcl2YH8E3_7884mcJ0DRN7STGqA8xZQKd-0MFmPftlxUR6i1xB9todMXRW2M6SIpXKAZ842UqKDm1UrkKG8nr7l9NjCkIw-GLQSFQ?auto=format&dpr=1&h=500&fr=1 1x, https://i.seadn.io/gae/WRcl2YH8E3_7884mcJ0DRN7STGqA8xZQKd-0MFmPftlxUR6i1xB9todMXRW2M6SIpXKAZ842UqKDm1UrkKG8nr7l9NjCkIw-GLQSFQ?auto=format&dpr=1&h=500&fr=1 2x',
+        title: 'Mint Genesis NFT',
+        price: '0.01 ETH',
+        tradingVolume: '68 ETH',
+    },
+    {
+        imageUrl: 'https://i.seadn.io/s/raw/files/c2343055844908c788fb0fac667d9063.jpg?auto=format&dpr=1&h=500&fr=1 1x, https://i.seadn.io/s/raw/files/c2343055844908c788fb0fac667d9063.jpg?auto=format&dpr=1&h=500&fr=1 2x',
+        title: 'Mint Genesis NFT',
+        price: '0.01 ETH',
+        tradingVolume: '68 ETH',
+    },
+    {
+        imageUrl: 'https://i.seadn.io/gcs/files/cd3e28d133070f314c751ccb1291a532.jpg?auto=format&dpr=1&h=500&fr=1 1x, https://i.seadn.io/gcs/files/cd3e28d133070f314c751ccb1291a532.jpg?auto=format&dpr=1&h=500&fr=1 2x',
+        title: 'Mint Genesis NFT',
+        price: '0.01 ETH',
+        tradingVolume: '68 ETH',
+    },
+    {
+        imageUrl: 'https://i.seadn.io/gcs/files/b3f8881dc097cc7de7bc7250622118e5.png?auto=format&dpr=1&h=500&fr=1',
+        title: 'Mint Genesis NFT',
+        price: '0.01 ETH',
+        tradingVolume: '68 ETH',
+    },
+    {
+        imageUrl: 'https://i.seadn.io/gcs/files/25059d629ad50cad3009a1f553a44401.jpg?auto=format&dpr=1&h=500&fr=1 1x, https://i.seadn.io/gcs/files/25059d629ad50cad3009a1f553a44401.jpg?auto=format&dpr=1&h=500&fr=1 2x',
+        title: 'Mint Genesis NFT',
+        price: '0.01 ETH',
+        tradingVolume: '68 ETH',
+    },
+]
 
 </script>
 
@@ -129,9 +182,12 @@ const selectType = (index: number) => {
             display: flex;
             justify-content: start;
             align-items: center;
+            
 
             font-size: 20px;
             font-weight: bold;
+
+            margin-top: 10px;
 
             .StatisticsContentDetailName {
                 display: flex;
