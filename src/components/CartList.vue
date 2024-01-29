424111533660154d4527d0ee23433ff9a51a5980
TECNB <3489044730@qq.com>
2024-01-29 22:18:00 +0800

    feat(CartList.vue):完善购物车功能
    - 实现一键清除所有的购物车商品
    - 实现点击商品从购物车中移除

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -12,14 +12,15 @@
         </div>
         <div class="Content">
             <p style="margin-top: 20%;" v-if="isCartNullVisible">还没有任何商品,快去购物吧</p>
-            <div class="Detail">
+            <div class="Detail" v-if="!isCartNullVisible">
                 <div class="DetailUp">
                     <p>{{ cartListLength }} 件商品</p>
-                    <p>清除所有</p>
+                    <p @click="clearAll">清除所有</p>
                 </div>
-                <div class="DetailBelow" v-for="(item, index) in cartList" :key="index">
-                    <div class="DetailBelowName">
-                        <div style="flex: 3;">
+                <div class="DetailBelow" v-for="(item, index) in cartList" :key="index" @mouseover="showDelete(index)"
+                    @mouseleave="hideDelete()">
+                    <div class="DetailBelowName" style="flex: 10;">
+                        <div class="Image" style="flex: 3;">
                             <img :src="item.imageUrl" alt=""
                                 style="height: 100%; width: 100%; border-radius: 20px; object-fit: cover; aspect-ratio: 1/1;">
                         </div>
@@ -29,20 +30,25 @@
                         </div>
 
                     </div>
-                    <div style="flex: 2;text-align: end;">{{ item.price }}</div>
 
-                    <!-- <div style="flex: 1;" class="icon"><el-icon>
-                            <Delete />
-                        </el-icon>
-                    </div> -->
+                    <div style="flex: 1;">
+                        <p v-if="!isDeleteVisible[index].value">{{ item.price }}</p>
+                        <div @click="deleteCart(index)" v-else>
+                            <el-icon>
+                                <Delete />
+                            </el-icon>
+                        </div>
+
+
+                    </div>
 
                 </div>
             </div>
-            <div class="Price">
+            <div class="Price" v-if="!isCartNullVisible">
                 <p>总记</p>
                 <p>${{ totalPrice }}</p>
             </div>
-            <div class="payMethod">
+            <div class="payMethod" v-if="!isCartNullVisible">
                 <p>支付方式</p>
 
                 <label>
@@ -64,14 +70,15 @@
 </template>
 
 <script setup lang="ts">
-import { } from "vue"
+import { ref } from "vue"
 import { Collection } from '../interfaces/Collection';
 
 const props = defineProps(['ifShow']);
 const emit = defineEmits();
 
-// 定义一个数组用于储存购物车的内容
-const cartList: Collection[] = [
+// 定义一个数组用于储存购物车的内容,并且为响应式
+// TODO:记得cartList一定是要放在全局变量里面的，否则清除所有就是清除不了的
+const cartList = ref<Collection[]>([
     {
         imageUrl: 'https://i.seadn.io/s/raw/files/6662e4fbea8ad15eb84990bc68351d57.png?auto=format&dpr=1&h=500&fr=1 1x, https://i.seadn.io/s/raw/files/6662e4fbea8ad15eb84990bc68351d57.png?auto=format&dpr=1&h=500&fr=1 2x',
         title: 'Mint Genesis NFT',
@@ -103,30 +110,66 @@ const cartList: Collection[] = [
         tradingVolume: '68 ETH',
     },
 
-]
+])
+
 // 定义一个变量用于控制是否展示购物车为空的情况
 let isCartNullVisible = false;
 // 定义一个变量用于储存cartList数组的总数
-let cartListLength = cartList.length;
+let cartListLength = cartList.value.length;
 // 定义一个变量用于储存购物车的总价
 let totalPrice = 0;
 // 通过cartList的price算出totalPrice，因为price是字符串类型，所以需要转换为数字类型
 // 同时需要把price后面的ETH去掉
-for (let i = 0; i < cartList.length; i++) {
-    totalPrice += Number(cartList[i].price.replace(' ETH', ''));
+for (let i = 0; i < cartList.value.length; i++) {
+    totalPrice += Number(cartList.value[i].price.replace(' ETH', ''));
 }
 // 最后保留两位小数
 totalPrice = Number(totalPrice.toFixed(2));
 
 
 // 如果cartList为空则显示还没有任何商品,快去购物吧
-if (cartList.length == 0) {
+if (cartList.value.length == 0) {
     isCartNullVisible = true
 }
 
 const toggleVisibility = () => {
     emit('updateIfShow', false);
 };
+
+// 实现clearAll方法将cartList清空
+const clearAll = () => {
+    cartList.value.splice(0, cartList.value.length);
+    cartListLength = cartList.value.length;
+    totalPrice = 0;
+    isCartNullVisible = true;
+}
+
+// 定义一个变量isDeleteVisible
+const isDeleteVisible = cartList.value.map(() => ref(false));
+// 实现showDelete方法
+const showDelete = (index: number) => {
+    isDeleteVisible.forEach((item, i) => (item.value = i === index));
+};
+// 实现hideDelete方法
+const hideDelete = () => {
+    isDeleteVisible.forEach((item) => (item.value = false));
+};
+// 实现Delete方法
+const deleteCart = (index: number) => {
+    cartList.value.splice(index, 1);
+    cartListLength = cartList.value.length;
+    totalPrice = 0;
+    // 通过cartList的price算出totalPrice，因为price是字符串类型，所以需要转换为数字类型
+    // 同时需要把price后面的ETH去掉
+    for (let i = 0; i < cartList.value.length; i++) {
+        totalPrice += Number(cartList.value[i].price.replace(' ETH', ''));
+    }
+    // 最后保留两位小数
+    totalPrice = Number(totalPrice.toFixed(2));
+    if (cartList.value.length == 0) {
+        isCartNullVisible = true
+    }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -196,15 +239,24 @@ const toggleVisibility = () => {
                 align-items: center;
 
 
+
                 font-size: 16px;
 
 
                 margin-top: 10px;
+                padding: 5px;
 
                 .DetailBelowName {
                     display: flex;
                     justify-content: center;
                     align-items: center;
+
+
+                    .Image {
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                    }
                 }
 
                 .icon {
@@ -213,6 +265,12 @@ const toggleVisibility = () => {
                     align-items: center;
 
                 }
+            }
+
+            .DetailBelow:hover {
+                border-radius: 20px;
+                height: 100%;
+                background-color: var(--accent-100);
             }
         }
 
@@ -236,6 +294,7 @@ const toggleVisibility = () => {
             font-weight: bold;
 
             margin-top: 10px;
+
             // radio选中时的颜色
             .radio {
                 accent-color: var(--accent-200);
