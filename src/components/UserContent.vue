a1d976353d5d362efb471baa21fb0d9ee927cd68
TECNB <3489044730@qq.com>
2024-01-30 13:07:11 +0800

    feat(UserContent.vue)：增加已收藏的UI
    - 已收藏中的内容以卡片的形式展示

--- src/components/UserContent.vue
+++ src/components/UserContent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="UserContent">
-        
+
         <div class="UserContentCollection" v-if="UserIndex.index == 0">
             <UserFilterSection />
             <p style="text-align: start;font-weight: bold;margin-bottom: 20px;">0个项目</p>
@@ -32,11 +32,19 @@
         </div>
 
         <div class="UserContentLiked" v-if="UserIndex.index == 4">
-            <!-- <div class="UserContentDetail">
-                <h2>没有要显示的项目</h2>
-                <button style="color: white;background-color: var(--accent-200);margin-top: 20px;">返回所有项目</button>
-            </div> -->
-            
+            <div v-for="(item, index) in collectionItems" :key="index" class="CollectionListItem">
+                <div class="CollectionListItemImage" style="height: 150px; width: 240px;">
+                    <img style="height: 100%; width: 100%; border-radius: 20px 20px 0px 0px; object-fit: cover;"
+                        :src="item.imageUrl" alt="" />
+                </div>
+
+                <p style="text-align: left; padding: 10px 20px;">{{ item.title }}</p>
+                <div class="CollectionListItemDetail">
+                    <p style="font-size: 16px; font-weight: normal;">交易价格</p>
+                    <p>{{ item.price }}</p>
+                </div>
+            </div>
+
         </div>
         <div class="UserContentActivity" v-if="UserIndex.index == 5">
             <!-- <div class="UserContentDetail">
@@ -61,7 +69,27 @@ import { SelectedUserIndexStore } from '../stores/SelectedIndexStore'
 import UserFilterSection from '../components/UserFilterSection.vue'
 
 const UserIndex = SelectedUserIndexStore()
+import { } from "vue"
 
+import { RecommendedCollectionStore } from '../stores/CollectionStore'
+import { Collection } from '../interfaces/Collection';
+
+
+
+
+// 像 useRouter 那样定义一个变量拿到实例
+const RecommendedCollection = RecommendedCollectionStore()
+
+
+let collectionItems: Collection[];
+collectionItems = RecommendedCollection.collections
+
+
+
+
+
+// 直接通过实例来获取数据
+console.log(RecommendedCollection.collections)
 
 </script>
 
@@ -69,17 +97,60 @@ const UserIndex = SelectedUserIndexStore()
 .UserContent {
     width: 100%;
 
-    .UserContentDetail {
+
+    .UserContentLiked {
         display: flex;
-        justify-content: center;
+        justify-content: start;
         align-items: center;
-        flex-direction: column;
-
+        flex-wrap: wrap;
+        gap: 20px;
         width: 100%;
-        height: 30vh;
+        height: 90%;
 
-        border: 0.5px solid var(--text-200);
-        border-radius: 20px;
+        font-weight: bold;
+
+        .CollectionListItem {
+            height: 100%;
+            width: 240px;
+            background-color: #FFFFFF;
+            border-radius: 20px;
+            box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
+            /* 调整阴影效果 */
+            transition: box-shadow 0.3s ease, transform 0.3s ease;
+
+
+
+            /* 添加过渡效果 */
+            .CollectionListItemDetail {
+                display: flex;
+                justify-content: start;
+                align-items: center;
+                gap: 20px;
+
+                text-align: left;
+                padding: 10px 20px;
+
+            }
+        }
+
+        .CollectionListItem:hover {
+            /* 鼠标悬停时的阴影效果 */
+            box-shadow: 0 20px 20px rgba(0, 0, 0, 0.2);
+        }
+
     }
+}
+
+.UserContentDetail {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    width: 100%;
+    height: 30vh;
+
+    border: 0.5px solid var(--text-200);
+    border-radius: 20px;
 }
 </style>
