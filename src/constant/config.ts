574c14e75bdcb6d367a79ac58a35fca742625aee
TECNB <3489044730@qq.com>
2024-04-22 13:39:44 +0800

    fix(config):修改腾讯云密钥

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -2,8 +2,8 @@
 
 // 使用 export default 导出一个对象，这个对象包含了一些配置参数
 export default {
-    SECRET_ID: "AKIDi1GauWPMKLVgnqot8QpCUkuz2DYvGlIm", // 密钥 ID
-    SECRET_KEY: "aJ6t6Z9mYJC0vcSWfrAXu27NUwZCsU4d", // 密钥
+    SECRET_ID: "AKIDNczHMDrIbjdJuJVM6jTIwSuQ1V0HTceW", // 密钥 ID
+    SECRET_KEY: "sY0LSP8dvDSV2PeuHwtos4ASwcO72meq", // 密钥
     action: "TextToImage", // 操作类型
     host: "aiart.tencentcloudapi.com", // 主机地址
     service: "aiart", // 服务类型
